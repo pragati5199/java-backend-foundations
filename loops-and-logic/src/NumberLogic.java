9c57ab80be159008a0ba10789b50ae8de65cd629
Prashant Saxena <your-email@gmail.com>
2026-01-21 20:22:59 +0530

    Refine digit sum and product logic with edge case handling

--- loops-and-logic/src/NumberLogic.java
+++ loops-and-logic/src/NumberLogic.java
@@ -1,6 +1,7 @@
 public class NumberLogic {
     static int reverse(int n) {
         int rev = 0;
+        n = Math.abs(n);
         while (n > 0) {
             rev = rev * 10 + (n % 10);
             n /= 10;
@@ -9,10 +10,12 @@ public class NumberLogic {
     }
 
     static boolean isPalindrome(int n) {
+        n = Math.abs(n);
         return n == reverse(n);
     }
 
     static int countDigits(int n) {
+        n = Math.abs(n);
         int count = 0;
         while (n > 0) {
             count++;
@@ -21,11 +24,43 @@ public class NumberLogic {
         return count;
     }
 
-    public static void main(String[] args) {
-        int num = 121;
+    static int sumOfDigits(int n) {
+        n = Math.abs(n);
+        int sum = 0;
 
-        System.out.println("Reverse: " + reverse(num));
-        System.out.println("Is Palindrome: " + isPalindrome(num));
-        System.out.println("Digit Count: " + countDigits(num));
+        while (n > 0) {
+            int rem = n % 10;
+            n /= 10;
+            sum  = sum + rem;
+        }
+        return sum;
+    }
+
+    static int productOfDigits(int n) {
+        n = Math.abs(n);
+        int product = 1;
+
+        if (n == 0) return 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+            n /= 10;
+            product  = product * rem;
+        }
+            return product;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {121,234,-7899,0,1,12345};
+
+        for (int num : input) {
+            System.out.println("----------------------------------------------");
+            System.out.println("Reverse: " + reverse(num));
+            System.out.println("Is Palindrome: " + isPalindrome(num));
+            System.out.println("Digit Count: " + countDigits(num));
+            System.out.println("Digit Sum: " + sumOfDigits(num));
+            System.out.println("Digit Product: " + productOfDigits(num));
+            System.out.println("----------------------------------------------");
+        }
     }
 }
